ae0c7f6938f9bea632a209c1be8ae39ac7019037
Suraj Shrestha <suraj.sht@hotmail.com>
2015-10-25 14:25:40 +0545

    third commit

--- app/build/generated/source/r/debug/com/example/lovecalculator/R.java
+++ app/build/generated/source/r/debug/com/example/lovecalculator/R.java
@@ -15,24 +15,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int autumnleaves=0x7f020000;
-        public static final int birds=0x7f020001;
-        public static final int blueflower2=0x7f020002;
-        public static final int blueflowers=0x7f020003;
-        public static final int digital_heart150=0x7f020004;
-        public static final int dock=0x7f020005;
-        public static final int forest=0x7f020006;
-        public static final int forestflowers=0x7f020007;
-        public static final int frangipaniflowers=0x7f020008;
-        public static final int garden=0x7f020009;
-        public static final int hill=0x7f02000a;
-        public static final int ic_launcher=0x7f02000b;
-        public static final int noname=0x7f02000c;
-        public static final int riverside=0x7f02000d;
-        public static final int rose=0x7f02000e;
-        public static final int sun=0x7f02000f;
-        public static final int waterfall=0x7f020010;
-        public static final int whiteflower=0x7f020011;
+        public static final int digital_heart150=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int rose=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
